919d889c6e3d30c6f7f0b973c071982ad59eacfd
TarasFedorenko <119684322+TarasFedorenko@users.noreply.github.com>
2024-04-29 15:17:22 +0300

    Added logging of GlobalExceptionHandler

--- src/main/java/com/clearsolution/restfulapitask/exception/GlobalExceptionHandler.java
+++ src/main/java/com/clearsolution/restfulapitask/exception/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.clearsolution.restfulapitask.exception;
 
+import com.clearsolution.restfulapitask.controller.UserController;
 import jakarta.validation.ConstraintViolationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -9,34 +12,40 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     @ExceptionHandler(UserAgeUnacceptableException.class)
     public ResponseEntity<String> handleUserAgeUnacceptableException(UserAgeUnacceptableException ex) {
+        logger.error("User Age Unacceptable Exception: {}", ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
+        logger.error("User Not Found Exception: {}", ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
     @ExceptionHandler(WrongDateRangeException.class)
     public ResponseEntity<String> handleWrongDateRangeException(WrongDateRangeException ex) {
+        logger.error("Wrong Date Range Exception: {}", ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException ex) {
+        logger.error("Illegal Argument Exception: {}", ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException ex) {
+        logger.error("Constraint Violation Exception: {}", ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        logger.error("Method Argument Not Valid Exception: {}", ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
